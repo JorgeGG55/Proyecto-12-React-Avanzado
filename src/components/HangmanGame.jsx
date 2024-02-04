3f1580a2248964151153033169340257742b7b51
Jorge Gravel <jorge.gravel.g@gmail.com>
2024-02-04 19:49:23 +0100

    useReducer

--- src/components/HangmanGame.jsx
+++ src/components/HangmanGame.jsx
@@ -1,68 +1,94 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer, useEffect } from 'react';
+
+const words = [
+  'car',
+  'computer',
+  'javascript',
+  'react',
+  'develop',
+  'videogame',
+  'cat',
+  'dog',
+  'page',
+  'house'
+];
+const maxAttempts = 6;
+
+const hangmanReducer = (state, action) => {
+  switch (action.type) {
+    case 'START_GAME':
+      const randomIndex = Math.floor(Math.random() * words.length);
+      const randomWord = words[randomIndex].toLowerCase();
+      return {
+        gameStarted: true,
+        selectedWord: randomWord,
+        displayedWord: '_'.repeat(randomWord.length),
+        usedLetters: [],
+        remainingAttempts: maxAttempts,
+        gameResult: ''
+      };
+    case 'HANDLE_LETTER_CLICK':
+      const { letter } = action;
+      if (state.usedLetters.includes(letter)) {
+        return state;
+      }
+
+      const newUsedLetters = [...state.usedLetters, letter];
+
+      if (state.selectedWord.includes(letter)) {
+        const newDisplayedWord = state.selectedWord
+          .split('')
+          .map((char, index) => (char === letter ? letter : state.displayedWord[index]))
+          .join('');
+        return { ...state, usedLetters: newUsedLetters, displayedWord: newDisplayedWord };
+      } else {
+        return {
+          ...state,
+          usedLetters: newUsedLetters,
+          remainingAttempts: state.remainingAttempts - 1
+        };
+      }
+    case 'HANDLE_GAME_RESTART':
+      return {
+        ...state,
+        ...hangmanReducer(undefined, { type: 'START_GAME' })
+      };
+    case 'UPDATE_GAME_RESULT':
+      if (state.displayedWord === state.selectedWord) {
+        return { ...state, gameResult: '¡You won!' };
+      } else if (state.remainingAttempts === 0) {
+        return { ...state, gameResult: `¡You lose! The word was: ${state.selectedWord}` };
+      }
+      return state;
+    default:
+      return state;
+  }
+};
 
 const HangmanGame = () => {
-  const words = [
-    'car',
-    'computer',
-    'javascript',
-    'react',
-    'develop',
-    'videogame',
-    'cat',
-    'dog',
-    'page',
-    'house'
-  ];
-  const maxAttempts = 6;
+  const [state, dispatch] = useReducer(hangmanReducer, {
+    gameStarted: false,
+    selectedWord: '',
+    displayedWord: '',
+    usedLetters: [],
+    remainingAttempts: maxAttempts,
+    gameResult: ''
+  });
 
-  const [gameStarted, setGameStarted] = useState(false);
-  const [selectedWord, setSelectedWord] = useState('');
-  const [displayedWord, setDisplayedWord] = useState('');
-  const [usedLetters, setUsedLetters] = useState([]);
-  const [remainingAttempts, setRemainingAttempts] = useState(maxAttempts);
-  const [gameResult, setGameResult] = useState('');
-
-  const startGame = () => {
-    const randomIndex = Math.floor(Math.random() * words.length);
-    const randomWord = words[randomIndex].toLowerCase();
-    setSelectedWord(randomWord);
-    setDisplayedWord('_'.repeat(randomWord.length));
-    setUsedLetters([]);
-    setRemainingAttempts(maxAttempts);
-    setGameStarted(true);
-    setGameResult('');
-  };
+  const { gameStarted, selectedWord, displayedWord, usedLetters, remainingAttempts, gameResult } =
+    state;
 
   const handleLetterClick = (letter) => {
-    if (usedLetters.includes(letter)) {
-      return;
-    }
-
-    const newUsedLetters = [...usedLetters, letter];
-    setUsedLetters(newUsedLetters);
-
-    if (selectedWord.includes(letter)) {
-      const newDisplayedWord = selectedWord
-        .split('')
-        .map((char, index) => (char === letter ? letter : displayedWord[index]))
-        .join('');
-      setDisplayedWord(newDisplayedWord);
-    } else {
-      setRemainingAttempts(remainingAttempts - 1);
-    }
+    dispatch({ type: 'HANDLE_LETTER_CLICK', letter });
   };
 
   const handleGameRestart = () => {
-    startGame();
+    dispatch({ type: 'HANDLE_GAME_RESTART' });
   };
 
   useEffect(() => {
     if (gameStarted) {
-      if (displayedWord === selectedWord) {
-        setGameResult('¡You won!');
-      } else if (remainingAttempts === 0) {
-        setGameResult(`¡You loose! The word was: ${selectedWord}`);
-      }
+      dispatch({ type: 'UPDATE_GAME_RESULT' });
     }
   }, [displayedWord, remainingAttempts, selectedWord, gameStarted]);
 
